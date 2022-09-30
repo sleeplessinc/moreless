99df8c01a7a825e5e5933d9d3561c16d631e4b9d
Joe Hitchens <joe@sleepless.com>
2022-09-30 16:18:31 -0700

    -

--- moreless.js
+++ moreless.js
@@ -5,17 +5,28 @@ const L = console.log;
 
         const LESS_LINES = 8;
 
+        const slide = function( el, target, cb ) {
+            const current = el.clientHeight;
+            const increment = ( target - current ) / 10;
+            const remainder = ( target - current ) % 10;
+            let tid = setInterval( () => {
+                const next = el.clientHeight + increment
+            }, 10 );
+        };
+
         const less = function( el ) {
-            el.style.height = ""+LESS_LINES+"em";
+
             el.style.overflow = "hidden";
+            el.style.height = ""+LESS_LINES+"em";
 
             const div = el.moreless_div;
             div.remove( div );
+            div.html( "Read more ..." );
             el.prepend( div );
-            //div.style.top = ""+(LESS_LINES - 1)+"em";
-            div.style.top = ( (el.clientHeight - div.clientHeight) + 1 ) + "px";
+            div.style.top = ((el.clientHeight - div.clientHeight) + 1) + "px";
 
             el.lessened = true;
+
         };
 
         const more = function( el ) {
@@ -24,8 +35,9 @@ const L = console.log;
 
             const div = el.moreless_div;
             div.remove( div );
-            div.style.top = "0";
+            div.html( "Read less ..." );
             el.appendChild( div );
+            div.style.top = "0";
 
             el.lessened = false;
         };
@@ -34,21 +46,21 @@ const L = console.log;
         els.forEach( el => {
 
             const div = document.createElement( "div" );
-            const style = div.style;
-            style.fontFamily = "sans-serif";
-            style.fontWeight = "bold";
-            style.color = "teal";
-            style.background = "linear-gradient(0deg, white, rgba(255,255,255,0.9), transparent)";
-            style.position = "relative";
-            style.padding = "2.0em 2vw 0.0em 2vw"
-            div.html( "Read more ..." );
+            div.style.cursor = "pointer";
+            div.style.fontFamily = "sans-serif";
+            div.style.fontWeight = "bold";
+            div.style.color = "teal";
+            div.style.background = "linear-gradient(0deg, white, rgba(255,255,255,0.9), transparent)";
+            div.style.position = "relative";
+            div.style.padding = "2.0em 2vw 0.0em 2vw"
             el.moreless_div = div;
 
             el.orig_height = el.style.height;
+            el.style.transition = "all 0.5s ease-out 0s";
 
             less( el );
 
-            el.onclick = evt => {
+            div.onclick = evt => {
                 if( el.lessened ) {
                     more( el );
                 } else {
