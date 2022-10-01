cd825c68bd0a4329d08f475c5ba763283fb584aa
Zafuzi <zacharyfoutz@gmail.com>
2022-09-30 17:12:12 -0700

    * fixed transition by using classes

--- moreless.js
+++ moreless.js
@@ -1,67 +1,37 @@
 const { log5, QS, QS1, o2j } = sleepless;
 const L = console.log;
 (function() {
-    window.onload = evt => {
-
-        const LESS_LINES = 8;
-
-        const slide = function( el, target, cb ) {
-            const current = el.clientHeight;
-            const increment = ( target - current ) / 10;
-            const remainder = ( target - current ) % 10;
-            let tid = setInterval( () => {
-                const next = el.clientHeight + increment
-            }, 10 );
-        };
-
+    window.onload = () => {
         const less = function( el ) {
-
-            el.style.overflow = "hidden";
-            el.style.height = ""+LESS_LINES+"em";
-
             const div = el.moreless_div;
-            div.remove( div );
-            div.html( "Read more ..." );
-            el.prepend( div );
-            div.style.top = ((el.clientHeight - div.clientHeight) + 1) + "px";
-
-            el.lessened = true;
-
+            div.innerText = "Read more ...";
+            el.style.height = "100px";
+            el.collapsed = true;
+            el.classList.add( "collapsed" );
         };
 
         const more = function( el ) {
-            el.style.height = el.orig_height;
-            el.style.overflow = "default";
-
             const div = el.moreless_div;
-            div.remove( div );
-            div.html( "Read less ..." );
-            el.appendChild( div );
-            div.style.top = "0";
-
-            el.lessened = false;
+            el.style.height = el.scrollHeight + "px";
+            div.innerText = "Read less ...";
+            
+            el.collapsed = false;
+            el.classList.remove( "collapsed" );
         };
 
-        const els = QS( ".moreless" );
+        const els = QS( ".moreLess" );
         els.forEach( el => {
 
             const div = document.createElement( "div" );
-            div.style.cursor = "pointer";
-            div.style.fontFamily = "sans-serif";
-            div.style.fontWeight = "bold";
-            div.style.color = "teal";
-            div.style.background = "linear-gradient(0deg, white, rgba(255,255,255,0.9), transparent)";
-            div.style.position = "relative";
-            div.style.padding = "2.0em 2vw 0.0em 2vw"
+            div.classList.add( "toggle" );
             el.moreless_div = div;
-
-            el.orig_height = el.style.height;
-            el.style.transition = "all 0.5s ease-out 0s";
+            
+            el.appendChild(div);
 
             less( el );
 
-            div.onclick = evt => {
-                if( el.lessened ) {
+            div.onclick = () => {
+                if( el.collapsed ) {
                     more( el );
                 } else {
                     less( el );
@@ -71,4 +41,3 @@ const L = console.log;
         } );
     };
 })();
-
