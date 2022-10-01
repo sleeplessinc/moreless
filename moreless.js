ad4f9270c753f79e3d5dbd63ddeafd4e123c3375
Joe Hitchens <joe@sleepless.com>
2022-10-01 15:37:41 -0700

    -

--- moreless.js
+++ moreless.js
@@ -12,23 +12,31 @@
         return toArray( document.querySelectorAll( qs ) );
     };
 
-    globalThis.moreless = function( sel = ".moreless", prmpts = [ "▼", "▲" ], prmpt_color = "teal" ) {
+    const DEF_OPTS = {
+        prompts: [ "▼", "▲" ],
+        prompt_color: "teal",
+        gradient: "#cccccccc",
+    };
+
+    globalThis.moreless = function( sel = ".moreless", opts = {} ) {
+
+        opts = Object.assign( {}, DEF_OPTS, opts );
 
         const less = function( el ) {
             const div = el.moreless_div;
-            div.innerText = prmpts[ 0 ];
-            div.style.background = "linear-gradient( rgba(255,255,255,0.1), rgba(255,255,255,0.9), rgba(255,255,255,1.0))";
+            div.innerText = opts.prompts[ 0 ];
+            div.style.background = opts.gradient; //"linear-gradient( rgba(255,255,255,0.1), rgba(255,255,255,0.9), rgba(255,255,255,1.0))";
             el.style.height = "8em";
-            el.style.paddingBottom = "0em";
+            //el.style.paddingBottom = "0em";
             el.collapsed = true;
         };
 
         const more = function( el ) {
             const div = el.moreless_div;
-            div.innerText = prmpts[ 1 ];
+            div.innerText = opts.prompts[ 1 ];
             div.style.background = "transparent";
             el.style.height = el.scrollHeight + "px";
-            el.style.paddingBottom = "1.5em";
+            //el.style.paddingBottom = "0.0em";
             el.collapsed = false;
         };
 
@@ -42,13 +50,13 @@
             el.style.transition = "height 0.25s ease-in-out";
             //el.style.border = "dashed 1px lightgray";
             el.style.overflow = "hidden";
-            el.style.paddingBottom = "2em";
+            //el.style.paddingBottom = "2em";
             el.style.height = "default";
 
             const div = document.createElement( "div" );
 
             div.style.padding = "2em 1em 0.5em 0";
-            div.style.color = prmpt_color;
+            div.style.color = opts.prompt_color;
             div.style.cursor = "pointer";
             div.style.position = "absolute";
             div.style.width = "100%";
@@ -75,5 +83,7 @@
 
         } );
     };
+
+    console.log( "moreless -- Copyright 2022 Sleepless Inc. All Rights Reserved" );
 
 })();
