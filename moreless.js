06bd4dc9cf535ce0a40348ae1b2c03801689305a
Joe Hitchens <joe@sleepless.com>
2022-10-01 13:46:17 -0700

    -

--- moreless.js
+++ moreless.js
@@ -2,28 +2,31 @@ const { log5, QS, QS1, o2j } = sleepless;
 const L = console.log;
 (function() {
     window.onload = () => {
+
         const less = function( el ) {
             const div = el.moreless_div;
-            div.innerText = "Read more ...";
-            el.style.height = "100px";
+            div.style.opacity = "0";
+            div.innerText = "▼ Read more";
+            el.style.height = "8em";
             el.collapsed = true;
             el.classList.add( "collapsed" );
+            setTimeout( () => { div.style.opacity = "1.0"; }, 200 );
         };
 
         const more = function( el ) {
             const div = el.moreless_div;
+            div.style.opacity = "0";
             el.style.height = el.scrollHeight + "px";
-            div.innerText = "Read less ...";
-            
+            div.innerText = "▲ Read less";
             el.collapsed = false;
             el.classList.remove( "collapsed" );
+            setTimeout( () => { div.style.opacity = "1.0"; }, 200 );
         };
 
         const els = QS( ".moreLess" );
         els.forEach( el => {
 
             const div = document.createElement( "div" );
-            div.classList.add( "toggle" );
             el.moreless_div = div;
             
             el.appendChild(div);
